c1783906c7cfe186614f369169ef8b83fac16629
patw <patw.hi@gmail.com>
2016-07-07 15:57:00 +0800

    Implement iOS program

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
 } from 'react-native';
+import MapView from 'react-native-maps';
+import Api from './src/api';
 
-class AppWeather extends Component {
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+export default class AppWeather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pin: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      city: '',
+      temperature: '',
+      description: '',
+    };
+  }
+
+  watchID = undefined;
+
+  componentWillMount() {
+    console.log('??????');
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        console.log(pos);
+        this.setState({
+          pin: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+        });
+
+        this.callApi(pos.coords.latitude, pos.coords.longitude);
+      },
+      (error) => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+      }
+    );
+
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      const newRegion = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.onRegionChangeComplete(newRegion);
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  onRegionChangeComplete(region) {
+    console.log(region);
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = region;
+    this.setState({
+      pin: {
+        latitude,
+        longitude,
+        latitudeDelta,
+        longitudeDelta,
+      },
+    });
+
+    this.callApi(latitude, longitude);
+  }
+
+  callApi(latitude, longitude) {
+    Api(latitude, longitude).then(data => {
+      this.setState(data);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <MapView
+          onRegionChangeComplete={(region) => { this.onRegionChangeComplete(region) }}
+          style={styles.map}
+          initialRegion={this.state.pin}
+          showsUserLocation={true}
+          followsUserLocation={true}
+        ></MapView>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>{this.state.city}</Text>
+          <Text style={styles.text}>{this.state.temperature}°C</Text>
+          <Text style={styles.text}>{this.state.description}</Text>
+        </View>
       </View>
     );
   }
@@ -35,18 +119,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  map: {
+    flex: 2,
+    marginTop: 30,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
   },
 });
 
